550e5fc1cac921e63eddeba32bed8c5d8af303a2
David Gonzalez <david.gonzalez@beeva.com>
2016-09-20 13:48:24 +0200

    Implement Splash Activity

---
+++ app/src/main/java/com/beeva/planningpoker/ui/splash/SplashPresenter.java
@@ -0,0 +1,39 @@
+package com.beeva.planningpoker.ui.splash;
+
+import com.beeva.planningpoker.Presenter;
+import com.beeva.planningpoker.manager.language.LanguageManager;
+import com.beeva.planningpoker.model.DataRepository;
+import javax.inject.Inject;
+
+/**
+ * Created by david.gonzalez on 20/9/16.
+ */
+
+public class SplashPresenter extends Presenter<SplashPresenter.View> {
+
+  @Inject DataRepository dataRepository;
+  @Inject LanguageManager languageManager;
+  private View view;
+
+  @Inject public SplashPresenter() {
+
+  }
+
+  @Override public void initialize() {
+    super.initialize();
+    view = getView();
+    
+    languageManager.changeLocale(dataRepository.getAppLanguage());
+  }
+
+  public void onStart() {
+    view.startAnimation();
+    view.startTimer();
+  }
+
+  public interface View extends Presenter.View {
+    void startAnimation();
+    void startTimer();
+    void navigateToMainActivity();
+  }
+}
